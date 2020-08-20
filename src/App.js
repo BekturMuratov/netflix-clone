305a05ca4e7c06beaeb4f4a56c72f6df8139250d
BekturMuratov <Muratov225@gmail.com>
2020-08-20 21:17:39 +0600

    get posters and add some styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Row from './components/Row'
+import request from './components/Request';
 
 function App() {
   return (
     <div className="App">
-      
+      <Row title = "Netflix originals" isLargeRow={true} fetchUrl={request.fetchNetflixOriginals} />
+      <Row title = "Trending now"  fetchUrl={request.fetchTrending} />
+      <Row title = "Top Rated"  fetchUrl={request.fetchTopRated} />
+      <Row title = "Action Movies"  fetchUrl={request.fetchActionMovies} />
+      <Row title = "Comedies Movies"  fetchUrl={request.fetchComedyMovies} />
+      <Row title = "Horror Movies"  fetchUrl={request.fetchHorrorMovies} />
+      <Row title = "Romance Movies"  fetchUrl={request.fetchRomanceMovies} />
+      <Row title = "Documentaries"  fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
